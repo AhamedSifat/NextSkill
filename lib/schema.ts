57879c57f403e718443c09988449aa49365ff98e
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-06 23:57:57 +0600

    course creation

--- lib/schema.ts
+++ lib/schema.ts
@@ -2,7 +2,19 @@ import { z } from 'zod';
 
 export const courseLevels = ['Beginner', 'Intermediate', 'Advanced'] as const;
 export const courseStatus = ['Draft', 'Published', 'Archived'] as const;
-
+export const courseCategories = [
+  'Development',
+  'Business',
+  'Finance',
+  'IT & Software',
+  'Office Productivity',
+  'Personal Development',
+  'Design',
+  'Marketing',
+  'Health & Fitness',
+  'Music',
+  'Teaching & Academics',
+] as const;
 export const courseSchema = z.object({
   title: z
     .string()
@@ -15,11 +27,9 @@ export const courseSchema = z.object({
 
   fileKey: z.string().min(1, { message: 'File is required' }),
 
-  price: z.coerce
-    .number()
-    .min(1, { message: 'Price must be a positive number' }),
+  price: z.number().min(1, { message: 'Price must be a positive number' }),
 
-  duration: z.coerce
+  duration: z
     .number()
     .min(1, { message: 'Duration must be at least 1 hour' })
     .max(500, { message: 'Duration must be at most 500 hours' }),
@@ -28,7 +38,7 @@ export const courseSchema = z.object({
     message: 'Level is required',
   }),
 
-  category: z.string().min(1, { message: 'Category is required' }),
+  category: z.enum(courseCategories, { message: 'Category is required' }),
 
   smallDescription: z
     .string()
@@ -45,3 +55,5 @@ export const courseSchema = z.object({
     message: 'Status is required',
   }),
 });
+
+export type CourseSchemaType = z.infer<typeof courseSchema>;

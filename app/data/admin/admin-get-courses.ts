5e823ff4d7a1d82fa3d72d4470e32b7326f40366
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-15 13:57:13 +0600

    (admin): add function to get all courses

---
+++ app/data/admin/admin-get-courses.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/lib/prisma';
+import { requireAdmin } from './require-admin';
+
+export const adminGetCourses = async () => {
+  await requireAdmin();
+  const data = await prisma.course.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+
+    select: {
+      id: true,
+      title: true,
+      smallDescription: true,
+      duration: true,
+      status: true,
+      level: true,
+      price: true,
+      fileKey: true,
+      slug: true,
+    },
+  });
+
+  return data;
+};
